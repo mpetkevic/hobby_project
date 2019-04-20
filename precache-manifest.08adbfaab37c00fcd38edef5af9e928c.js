872ed5d0a11f9a1fba72d3613dd1b3fd460cc8f0
mpetkevic <miroslav.petkevic@gmail.com>
2019-04-20 16:06:55 +0300

    Updates

---
+++ precache-manifest.08adbfaab37c00fcd38edef5af9e928c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac5f0a4ba646f586332",
+    "url": "/hobby_project/static/js/runtime~main.0cacbdea.js"
+  },
+  {
+    "revision": "b7742c120b8d27409419",
+    "url": "/hobby_project/static/js/main.7fa17044.chunk.js"
+  },
+  {
+    "revision": "57df60d2b666b1e3fbfb",
+    "url": "/hobby_project/static/js/2.3d28c304.chunk.js"
+  },
+  {
+    "revision": "b7742c120b8d27409419",
+    "url": "/hobby_project/static/css/main.156fdc69.chunk.css"
+  },
+  {
+    "revision": "57df60d2b666b1e3fbfb",
+    "url": "/hobby_project/static/css/2.3908bb5c.chunk.css"
+  },
+  {
+    "revision": "54f29651417bef797f515e03e835a71e",
+    "url": "/hobby_project/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hobby_craft/precache-manifest.d090f226e9eaaa2a1b8693ab5805949b.js"
+  "/hobby_project/precache-manifest.08adbfaab37c00fcd38edef5af9e928c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hobby_craft/index.html", {
+workbox.routing.registerNavigationRoute("/hobby_project/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
