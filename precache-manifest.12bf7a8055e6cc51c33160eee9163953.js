c902016db43a09b24e7d47cdd80651a08885d3c0
mpetkevic <miroslav.petkevic@gmail.com>
2019-04-20 16:00:16 +0300

    Updates

---
+++ precache-manifest.12bf7a8055e6cc51c33160eee9163953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b110a83dc2248622fbc3",
+    "url": "/hobby_craft/static/js/runtime~main.a446c92d.js"
+  },
+  {
+    "revision": "15cafa916b64f4ed86bf",
+    "url": "/hobby_craft/static/js/main.d146d925.chunk.js"
+  },
+  {
+    "revision": "57df60d2b666b1e3fbfb",
+    "url": "/hobby_craft/static/js/2.3d28c304.chunk.js"
+  },
+  {
+    "revision": "15cafa916b64f4ed86bf",
+    "url": "/hobby_craft/static/css/main.156fdc69.chunk.css"
+  },
+  {
+    "revision": "57df60d2b666b1e3fbfb",
+    "url": "/hobby_craft/static/css/2.3908bb5c.chunk.css"
+  },
+  {
+    "revision": "a36bab0dff5d0da71093d8cb5f3d009c",
+    "url": "/hobby_craft/index.html"
+  }
+];
