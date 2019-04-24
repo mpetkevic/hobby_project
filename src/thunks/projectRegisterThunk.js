f0593ee2fed4fafd98bc0b71551adf34a5e502b8
mpetkevic <miroslav.petkevic@gmail.com>
2019-04-24 09:39:51 +0300

    Formatted Date & added to POST JSON

--- src/thunks/projectRegisterThunk.js
+++ src/thunks/projectRegisterThunk.js
@@ -1,19 +1,24 @@
 import * as actions from '../actions/projectRegisterActions';
+import moment from 'moment';
 import axios from 'axios';
 
 export const onProjectRegisterFormSubmit = (projectInfo, history) => (dispatch) => {
   dispatch(actions.onProjectRegisterFormLoading());
   if(projectInfo.username === '' || projectInfo.hobbyName === '' || projectInfo.email === '' ||
-  projectInfo.description === '' || projectInfo.amount === '' || projectInfo.date === null) {
+  projectInfo.description === '' || projectInfo.amount === '' || projectInfo.endDate === null) {
     return dispatch(actions.onProjectRegisterFormError('Please fill all fields'))
   }
+  const formattedDate = moment(projectInfo.endDate).format('YYYY-MM-DD');
+
   const json = JSON.stringify({
     "title": projectInfo.hobbyName,
     "description": projectInfo.description,
     "amount": projectInfo.amount,
     "email": projectInfo.email,
-    "username": projectInfo.username
-});
+    "username": projectInfo.username,
+    "date": formattedDate
+  });
+  console.log(json)
   axios.post('https://hobby-api.herokuapp.com/api',json)
     .then(res=>{
       dispatch(actions.onProjectRegisterFormSubmit(res));
