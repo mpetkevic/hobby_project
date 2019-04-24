16d3999dc3ec3a426e50600afa241fba1efa679e
mpetkevic <miroslav.petkevic@gmail.com>
2019-04-24 10:33:05 +0300

    Updates

---
+++ precache-manifest.2b32189de6c8d09eb5b66a92349cd2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fac5f0a4ba646f586332",
+    "url": "/hobby_project/static/js/runtime~main.0cacbdea.js"
+  },
+  {
+    "revision": "a3c9215adf5441a730b2",
+    "url": "/hobby_project/static/js/main.93820f87.chunk.js"
+  },
+  {
+    "revision": "8eb2efe7e1f7d02d6ae5",
+    "url": "/hobby_project/static/js/2.716b7823.chunk.js"
+  },
+  {
+    "revision": "a3c9215adf5441a730b2",
+    "url": "/hobby_project/static/css/main.6c0d165b.chunk.css"
+  },
+  {
+    "revision": "8eb2efe7e1f7d02d6ae5",
+    "url": "/hobby_project/static/css/2.2e7af592.chunk.css"
+  },
+  {
+    "revision": "21a4225d1fba8b35ede92c09da44192c",
+    "url": "/hobby_project/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hobby_project/precache-manifest.65be658e91b4b3bc0dee5b6aa9e1c6d0.js"
+  "/hobby_project/precache-manifest.2b32189de6c8d09eb5b66a92349cd2b2.js"
 );
 
 workbox.clientsClaim();
